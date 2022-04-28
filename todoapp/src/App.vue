b3ba13cd9a3b0365a0548abdec27e9ea0d54db89
uqingu <uqingu@foxmail.com>
2022-04-28 15:14:03 +0800

    测试脚手架

---
+++ todoapp/src/App.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <AppHearder :addTodo="addTodo" />
+        <AppList
+          :todos="todos"
+          :checkTodo="checkTodo"
+          :deleteTodo="deleteTodo"
+        />
+        <AppFooter
+          :todos="todos"
+          :checkAllTodo="checkAllTodo"
+          :clearAllTodo="clearAllTodo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHearder from "./components/AppHearder.vue";
+import AppList from "./components/AppList.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHearder,
+    AppList,
+    AppFooter,
+  },
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "编程", done: false },
+        { id: "002", title: "看书", done: true },
+        { id: "003", title: "听歌", done: true },
+        { id: "004", title: "上课", done: false },
+        { id: "005", title: "游戏", done: false },
+      ],
+    };
+  },
+  mounted() {
+    var css =`
+            background-image: linear-gradient(to right, orange, purple);
+            -webkit-background-clip: text;
+            color: white;
+            font-size: 15px;
+            border-radius:5px;`
+    console.log("%c我的测试 app", css);
+  },
+  methods: {
+    //添加一个todo
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj);
+    },
+    //勾选or取消勾选一个todo
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) todo.done = !todo.done;
+      });
+    },
+    //删除一个todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    //全选or取消全选
+    checkAllTodo(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done;
+      });
+    },
+    //清除所有已经完成的todo
+    clearAllTodo() {
+      this.todos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/*base*/
+body {
+  background: #fff;
+}
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+.btn:focus {
+  outline: none;
+}
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ todoapp/src/components/AppList.vue
@@ -0,0 +1,41 @@
+<template>
+<ul class="todo-main">
+		<AppItem
+			v-for="todoObj in todos"
+			:key="todoObj.id"
+			:todo="todoObj"
+			:checkTodo="checkTodo"
+			:deleteTodo="deleteTodo"
+		/>
+	</ul>
+</template>
+
+<script>
+import  AppItem from "./AppItem"
+export default {
+    name:"AppList",
+    props:['todos','checkTodo','deleteTodo'],
+    components:{
+        AppItem
+    }
+};
+</script>
+
+<style scoped>
+/*main*/
+	.todo-main {
+		margin-left: 0px;
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		padding: 0px;
+	}
+
+	.todo-empty {
+		height: 40px;
+		line-height: 40px;
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		padding-left: 5px;
+		margin-top: 10px;
+	}
+</style>
