a44f38f5b127232af75478ed1c8118482c88512b
uqingu <uqingu@foxmail.com>
2022-04-28 23:52:16 +0800

    test vue-markdown mavonEditor

---
+++ testapp/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="test">
+      <!-- <mavon-editor
+        v-model="value"
+        :ishljs="true"
+        :toolbarsFlag="false"
+        :subfield="false"
+      /> -->
+      <div class="markdown-body">
+        <VueMarkdown :source="value" v-highlight></VueMarkdown>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown";
+
+export default {
+  name: "App",
+  data() {
+    return { value: "# 测试标题\n## 测试标题" };
+  },
+  components: {
+    VueMarkdown,
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.test {
+  width: 800px;
+  position: absolute;
+  top: 300px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+</style>
