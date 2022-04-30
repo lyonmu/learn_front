62f8a2ef6451856d1300ad9270a2541af23af183
uqingu <uqingu@foxmail.com>
2022-04-30 14:36:20 +0800

    use Vuex

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div id="app">
+    <!-- <div id="app"> -->
       <img alt="Vue logo" src="./assets/logo.png" />
-    </div>
+    <!-- </div> -->
     <MarkdownDetail />
   </div>
 </template>
