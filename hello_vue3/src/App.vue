e290c52d49760e91be1f7b3176d57116da020630
uqingu <uqingu@foxmail.com>
2022-05-04 20:26:02 +0800

    <script setup>

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,18 +1,23 @@
-<script setup>
-
+<script setup >
+// https://v3.cn.vuejs.org/api/sfc-script-setup.html
+const name = "uqingu"
+const age = 18
+const sex = true
+function testfun(params) {
+  console.log(params)
+}
 </script>
 
 <template>
   <h1>HelloWorld</h1>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>性别：{{ sex }}</h2>
+  <h1>
+    <div @click="testfun(name)">点击触发函数</div>
+  </h1>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- hello_vue3/vue.config.js
+++ hello_vue3/vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    lintOnSave: false, //关闭语法检查
+    // lintOnSave: false, //关闭语法检查
+    lintOnSave: true, //关闭语法检查
 }
