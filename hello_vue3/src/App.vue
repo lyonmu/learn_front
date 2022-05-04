ebc70ec7dd5c70fbd981321a0f2bd15db4ee2228
uqingu <uqingu@foxmail.com>
2022-05-04 23:15:58 +0800

    ref, reactive, onMounted, onUpdated

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,10 +1,28 @@
 <script setup >
 // https://v3.cn.vuejs.org/api/sfc-script-setup.html
-const name = "uqingu"
-const age = 18
-const sex = true
+import { ref, reactive, onMounted, onUpdated } from "vue"
+const name = ref("uqingu")
+const age = ref(18)
+const sex = ref(true)
+const user = reactive({
+  id: "123456",
+  load: "南阳"
+})
 function testfun(params) {
   console.log(params)
+}
+// mounted
+onMounted(() => {
+  console.log("测试生命周期函数")
+})
+onUpdated(() => {
+  console.log("数据发生改变")
+})
+function testfun01() {
+  name.value = 'muqing'
+  age.value = 22
+  user.id = '654321'
+  user.load = "chedu"
 }
 </script>
 
@@ -14,8 +32,13 @@ function testfun(params) {
   <h2>姓名：{{ name }}</h2>
   <h2>年龄：{{ age }}</h2>
   <h2>性别：{{ sex }}</h2>
+  <h2>性别：{{ user.id }}</h2>
+  <h2>性别：{{ user.load }}</h2>
   <h1>
     <div @click="testfun(name)">点击触发函数</div>
+  </h1>
+  <h1>
+    <div @click="testfun01">点击修改属性函数</div>
   </h1>
 </template>
 
