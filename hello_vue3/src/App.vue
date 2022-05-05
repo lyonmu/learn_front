6fe4eb6fdfee07555685ec7755b5c2c7bdaea0fc
uqingu <uqingu@foxmail.com>
2022-05-05 12:17:44 +0800

    watch,computed

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,6 +1,6 @@
 <script setup >
 // https://v3.cn.vuejs.org/api/sfc-script-setup.html
-import { ref, reactive, onMounted, onUpdated } from "vue"
+import { ref, reactive, onMounted, onUpdated, watch, computed } from "vue"
 const name = ref("uqingu")
 const age = ref(18)
 const sex = ref(true)
@@ -18,12 +18,19 @@ onMounted(() => {
 onUpdated(() => {
   console.log("数据发生改变")
 })
+watch([name, user], (newValue, oldValue) => {
+  console.log(newValue + "===" + oldValue)
+  console.log(newValue + "===" + oldValue)
+})
 function testfun01() {
   name.value = 'muqing'
   age.value = 22
   user.id = '654321'
   user.load = "chedu"
 }
+const compu = computed(() => {
+  return name.value + user.load
+})
 </script>
 
 <template>
@@ -34,6 +41,7 @@ function testfun01() {
   <h2>性别：{{ sex }}</h2>
   <h2>性别：{{ user.id }}</h2>
   <h2>性别：{{ user.load }}</h2>
+  <h2>性别：{{ compu }}</h2>
   <h1>
     <div @click="testfun(name)">点击触发函数</div>
   </h1>
