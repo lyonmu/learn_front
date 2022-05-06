8406c6cee09b1a07fc166eb3987df1d2348f4d09
uqingu <uqingu@foxmail.com>
2022-05-06 12:07:05 +0800

    learn vue3 + ts + vite

---
+++ vue3_ts/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Demo from "./components/Demo.vue"
+import Demo1 from "./components/Demo.vue";
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Demo msg="test vite ts"/>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3_ts/src/components/Demo.vue
@@ -0,0 +1,39 @@
+<!--  -->
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <h1>{{ one.id }}</h1>
+        <h1>{{ one.load }}</h1>
+        <h1>{{ one.age }}</h1>
+        <h1>{{ one.sex }}</h1>
+        <button @click="testfun">点击修改属性</button>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { reactive, onMounted, onUpdated } from "vue"
+defineProps<{ msg: string }>()
+interface user {
+    id: string;
+    load: string;
+    age: number;
+    sex: boolean;
+}
+
+let one: user = reactive({
+    id: "uqingu", load: "nanyang", age: 19, sex: true
+})
+// mounted
+onMounted(() => {
+    console.log("测试生命周期函数")
+})
+onUpdated(() => {
+    console.log("数据发生改变")
+})
+function testfun() {
+    one.id = "test"
+    one.sex = !one.sex
+}
+</script>
+<style scoped>
+</style>
