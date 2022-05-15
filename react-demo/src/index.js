586719067754af1dcc3bd89b3b17996cde16f625
uqingu <uqingu@foxmail.com>
2022-05-15 21:03:59 +0800

    First React Component

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,11 +1,6 @@
 import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-  <div>
-    <h1>hello world</h1>
-  </div>
-);
-
-console.log("first");
-console.log("second");
+root.render(<App />);
